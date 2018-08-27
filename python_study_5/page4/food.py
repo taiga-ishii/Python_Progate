e121532b2b558aa80e863caee683146e4a87ad67
taiga-ishii <tiger1410111@gmail.com>
2018-08-27 20:34:26 +0900

    python5

---
+++ python_study_5/page4/food.py
@@ -0,0 +1,9 @@
+from menu_item import MenuItem
+
+class Food(MenuItem):
+    # calorie_infoメソッドを定義してください
+    def calorie_info(self):
+        print(str(self.calorie) + 'kcalです')
+    
+        
+    
